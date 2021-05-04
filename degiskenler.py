306226848bd32d530323ab701576838ab86c2d4e
Ege Paksoy <egepaskoy@gmail.com>
2021-05-04 16:46:31 +0300

    Derse başladım

---
+++ degiskenler.py
@@ -0,0 +1,57 @@
+# ----0.00----
+print("merhaba dünya")
+
+
+# ----1.37----
+num1 = 32
+num2 = -6
+
+tot = num1 + num2
+
+print("işlemin sonucu: ", tot)# + str(tot)
+
+
+# ----3.52----
+num1 = 32
+num2 = -6
+name = "ege"
+
+tot = num1 + num2
+
+print("merhaba" + name)
+
+
+# ----4.27----
+num1 = 32
+num2 = -6
+name = "ege"
+
+tot = num1 + num2
+
+print(f"merhaba {name}")# {tot}
+
+
+# ----4.54----
+num1 = 21.3
+num2 = -6
+name = "ege"
+
+tot = num1 + num2
+
+print(f"merhaba {tot}")
+
+
+# ----5.16----
+true = True
+false = 0
+
+print(int(true), bool(false))
+
+
+# ----6.05----
+int_num = 32
+str_num = '32'
+
+print(str(int_num) + str_num)
+
+
