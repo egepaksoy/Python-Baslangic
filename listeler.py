8c882ab629ca992f0dfe1f252deaa07efdf8e9de
Ege Paksoy <egepaskoy@gmail.com>
2021-05-06 22:13:33 +0300

    array videosunun kodları

---
+++ listeler.py
@@ -0,0 +1,48 @@
+# ----
+# liste = []
+# print(liste)
+
+# -----
+# ekmek = "ekmek"
+# liste = [ekmek]
+
+# print(liste)
+
+# ------
+# liste = ["ekmek", "soğan", "yumurta", "patates"]
+
+# print(liste[0], liste[2])
+
+# ---------
+# liste = ["ekmek", "soğan", "yumurta", "patates"]
+
+# liste.append("tuvalet kağıdı")
+
+# print(liste)
+
+# -----
+# liste = ["ekmek", "soğan", "yumurta", "patates"]
+
+# print(liste[len(liste)-1])
+# ----
+# liste = ["ekmek", "soğan", "yumurta", "patates"]
+
+# print(len(liste[len(liste)-1]), liste[len(liste)-1][len(liste[len(liste)-1])-1])
+# -----
+# students = [["ege", "paksoy"]]
+
+# name = input("Adınızı Girin: ")
+# surname = input("Soyadınızı Girin: ")
+
+# students.append([name, surname])
+
+# print(students)
+# --------
+# students = [["ege", "paksoy"]]
+
+# name = input("Adınızı Girin: ")
+# surname = input("Soyadınızı Girin: ")
+
+# students.append([name, surname])
+
+# print(students[1][0])
